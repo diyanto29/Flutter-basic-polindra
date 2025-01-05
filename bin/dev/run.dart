95d3f97076f8c94855f14e2ce1b4fab9017359fb
diyanto29 <diyanto2911@gmail.com>
2025-01-05 19:55:41 +0700

    Fixing

--- bin/dev/run.dart
+++ bin/dev/run.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'dart:mirrors';
-import 'package:dio/dio.dart';
 import '../chapter1.dart';
 import 'util.dart';
 
@@ -11,70 +10,54 @@ void runChapters({
 }) {
   bool owner = Directory("c:/yo").existsSync();
   if (!owner) {
-    if (fullName.isEmpty || whatsapp.isEmpty || email.isEmpty) {
-      printRed("[INFO]");
-      printGreen("""
-Silahkan isi:
-- fullName
-- whatsapp
-- email
-* Utk keperluan leaderboard & scoring
-* Boleh di isi - saja jika keberatan
-(Tapi utk member wajib ya)   
+//     if (fullName.isEmpty || whatsapp.isEmpty || email.isEmpty) {
+//       printRed("[INFO]");
+//       printGreen("""
+// Silahkan isi:
+// - fullName
+// - whatsapp
+// - email
+// * Utk keperluan leaderboard & scoring
+// * Boleh di isi - saja jika keberatan
+// (Tapi utk member wajib ya)
 
-Isi di bin/magicbook_basic.dart
-    """
-          .trim());
-      printRed("---------------");
-      exit(0);
+// Isi di bin/magicbook_basic.dart
+//     """
+//           .trim());
+//       printRed("---------------");
+//       exit(0);
+//     }
+//   } else {
+//     fullName = "Diyanto";
+//     whatsapp = "085624277920";
+//     email = "";
+//   }
+
+    int point = 0;
+
+    Chapter1 chapter1 = Chapter1();
+    InstanceMirror instanceMirror = reflect(chapter1);
+
+    var correctAnswers = [];
+    var wrongAnswers = [];
+    for (var i = 1; i <= 130; i++) {
+      var res = instanceMirror.invoke(Symbol("exercise$i"), []).reflectee;
+      point += res == true ? 1 : 0;
+      if (res) correctAnswers.add(i);
+      if (!res) wrongAnswers.add(i);
     }
-  } else {
-    fullName = "Diyanto";
-    whatsapp = "085624277920";
-    email = "";
-  }
 
-  int point = 0;
+    printGreen("Correct Answers:");
+    printGreen(correctAnswers.join(","));
+    printGreen("---");
 
-  Chapter1 chapter1 = Chapter1();
-  InstanceMirror instanceMirror = reflect(chapter1);
+    printRed("Wrong Answers:");
+    printRed(wrongAnswers.join(","));
+    printRed("---");
 
-  var correctAnswers = [];
-  var wrongAnswers = [];
-  for (var i = 1; i <= 130; i++) {
-    var res = instanceMirror.invoke(Symbol("exercise$i"), []).reflectee;
-    point += res == true ? 1 : 0;
-    if (res) correctAnswers.add(i);
-    if (!res) wrongAnswers.add(i);
-  }
+    printGreen("~~~");
+    printGreen("Point: $point");
 
-  printGreen("Correct Answers:");
-  printGreen(correctAnswers.join(","));
-  printGreen("---");
-
-  printRed("Wrong Answers:");
-  printRed(wrongAnswers.join(","));
-  printRed("---");
-
-  printGreen("~~~");
-  printGreen("Point: $point");
-
-  try {
-    // Dio().post(
-    //   "https://capekngoding.com/magicbook/api/scores",
-    //   options: Options(
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   ),
-    //   data: {
-    //     "name": fullName,
-    //     "email": email,
-    //     "whatsapp": whatsapp,
-    //     "point": point,
-    //   },
-    // );
-  } on Exception catch (err) {
-    print("--- 101 ---");
+  
   }
 }
